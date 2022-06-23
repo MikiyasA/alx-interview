ea9c32def16a26ae25b643d54e9e26ee96b7086b
Mikiyas Adere <mikiyasad86@gmail.com>
2022-06-23 23:19:40 +0000

    To try the only task

---
+++ 0x00-python_variable_annotations/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+"""
+
+
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened. """
+    n = len(boxes)
+    myList = [0]
+    for i in myList:
+        for j in boxes[i]:
+            if j not in myList:
+                if j < n:
+                    myList.append(j)
+    if len(myList) == n:
+        return True
+    return False
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+readme file for project 0x00-python_variable_annotations
