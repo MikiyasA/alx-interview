450fd2525da43bf0eba44f71d38ea323cc80231e
Mikiyas Adere <mikiyasad86@gmail.com>
2022-07-14 21:47:58 +0000

    To commit the task

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+method that determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """ Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    bytesLong = 0
+    binaryMoveLeft7 = 1 << 7
+    binaryMoveLeft6 = 1 << 6
+    for byte in data:
+        binaryMove = 1 << 7
+        if bytesLong == 0:
+            while byte & binaryMove:
+                bytesLong += 1
+                binaryMove = binaryMove >> 1
+            if bytesLong == 0:
+                continue
+            if bytesLong == 1 or bytesLong > 4:
+                return False
+        else:
+            if not (byte & binaryMoveLeft7 and not (byte & binaryMoveLeft6)):
+                return False
+        bytesLong -= 1
+    if bytesLong == 0:
+        return True
+    return False
